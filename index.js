9cb372fec91ba9b3f67a5aa4be10ddd3b611f5eb
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-08-30 15:55:03 +0530

    now it can brute force it as well

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 let type = document.getElementById("Slt");
+let text = document.getElementById("txt");
 
-if(type.value=="encrypt"){
-    document.querySelector(".Brute").classList.toggle("visible");
+function change(){
+    if(type.value=="Encrypt"){
+        document.querySelector(".brute").classList.add("visible");
+    }
 }
 
 function trigger() {
-    let text = document.getElementById("txt").value;
     let key = Number(document.getElementById("key").value);
     switch (type.value) {
         case "Encrypt":
-            cipher(text,key); 
+            document.querySelector(".crack").innerHTML=cipher(text.value,key); 
             break;
         case "Decrypt":
-            cipher(text,(26 - key));
+            document.querySelector(".crack").innerHTML=cipher(text.value,(26 - key));
             break;
     }
 }
@@ -22,7 +24,6 @@ function cipher(text,key) {
     let res = "";
     for (let i = 0; i < text.length; i++) {
         let char = text[i];
-        console.log(char);
         if (char.match(/[A-z," "]/g)) {
             let code = Number(char.charCodeAt(0));
             if(char ===" "){
@@ -38,6 +39,11 @@ function cipher(text,key) {
             }
         }
     }
-    console.log(res);
     return res;
+}
+
+function brute_force(){
+    for (let i = 1; i <= 26; i++) {
+        document.querySelectorAll(".result")[i].innerHTML= i+"."+cipher(text.value,(26-i));
+    }
 }
